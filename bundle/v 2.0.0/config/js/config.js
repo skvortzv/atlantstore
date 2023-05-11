752fe8a756b432aec23aed1b60deb1133f1043ca
skvortz <130127050+skvortzv@users.noreply.github.com>
2023-05-11 20:05:27 +0500

    Add files via upload

---
+++ bundle/v 2.0.0/config/js/config.js
@@ -0,0 +1,7 @@
+function addClass(whomClassAdd, whichClassAdd) {
+  $(`.${ whomClassAdd }`).addClass(whichClassAdd);
+}
+
+function removeClass(whomClassRemove, whichClassRemove) {
+  $(`.${ whomClassRemove }`).removeClass(whichClassRemove);
+}
