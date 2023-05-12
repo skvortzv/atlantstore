f0154552047b4addbe705b4e02cc58045155da0d
skvortz <130127050+skvortzv@users.noreply.github.com>
2023-05-12 16:30:53 +0500

    Delete common.blocks directory

--- common.blocks/mask/_visibled/mask_visibled.js
+++
@@ -1,9 +0,0 @@
-function maskVisibled() {
-  if($(".mask").hasClass("mask_visibled")) {
-    $("body").css("overflow-y", "hidden");
-  } 
-  
-  else {
-    $("body").css("overflow-y", "auto");
-  }
-}
